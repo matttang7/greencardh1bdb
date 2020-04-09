632675b0eee991cc28e928feea1ef1206b7ca4e6
matttang7 <mt13@illinois.edu>
2020-04-08 21:29:13 -0500

    initial work connecting to sql and inserting/deleting from the webapp

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(cors());
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'cs411pw',
+    database : 'cs411'
+  });
+  connection.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected with mysql database...')
+  })  
+  app.use( bodyParser.json() );       // to support JSON-encoded bodies
+  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }));
+
+  var server = app.listen(3000, "127.0.0.1", function () {
+    var host = server.address().address
+    var port = server.address().port 
+    console.log("Example app listening at http://%s:%s", host, port)
+  });
+app.get('/', (req, res) => {
+    console.log("reached");
+    connection.query('SELECT* FROM greencard WHERE Category="IT Jobs"', function(err, rows, fields) { 
+        if (!err) {
+            console.log("SENT")
+            res.send(rows);
+        } else {
+            console.log(err);
+            console.log('Error while performing Query.');
+        }
+    });
+});
+  //route for insert data
+app.post('/',(req, res) => {
+    let data = {Id: req.body.id, Title: req.body.title, FullDescription: req.body.fulldescription, LocationRaw: req.body.locationraw, LocationNormalized: req.body.locationnormalized, 
+      ContractType: req.body.contracttype, ContractTime: req.body.contracttime, Company: req.body.company, Category: req.body.category, SourceName: req.body.sourcename};
+    let sql = "INSERT INTO greencard SET ?";
+    console.log("reached");
+    let query = connection.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+  });
---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import axios from 'axios';
+import { JsonToTable } from "react-json-to-table";
+class App extends React.Component {
+ constructor(props) {
+  super(props)
+         this.state = {
+          allentries: []
+        }
+
+        this.handleIdChange = this.handleIdChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleFullDescriptionChange = this.handleFullDescriptionChange.bind(this);
+        this.handleLocationRawChange = this.handleLocationRawChange.bind(this);
+        this.handleLocationNormalizedChange = this.handleLocationNormalizedChange.bind(this);
+        this.handleContractTypeChange = this.handleContractTypeChange.bind(this);
+        this.handleContractTimeChange = this.handleContractTimeChange.bind(this);
+        this.handleCompanyChange = this.handleCompanyChange.bind(this);
+        this.handleCategoryChange = this.handleCategoryChange.bind(this);
+        this.handleSourceNameChange = this.handleSourceNameChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+      }
+
+      handleIdChange(e) {
+        this.setState({id: e.target.value})
+      }
+      handleTitleChange(e) {
+        this.setState({title: e.target.value})
+      }
+      handleFullDescriptionChange(e) {
+        this.setState({fulldescription: e.target.value})
+      }
+      handleLocationRawChange(e) {
+        this.setState({locationraw: e.target.value})
+      }
+      handleLocationNormalizedChange(e) {
+        this.setState({locationnormalized: e.target.value})
+      }
+      handleContractTypeChange(e) {
+        this.setState({contracttype: e.target.value})
+      }
+      handleContractTimeChange(e) {
+        this.setState({contracttime: e.target.value})
+      }
+      handleCompanyChange(e) {
+        this.setState({company: e.target.value})
+      }
+      handleCategoryChange(e) {
+        this.setState({category: e.target.value})
+      }
+      handleSourceNameChange(e) {
+        this.setState({sourcename: e.target.value})
+      }
+  componentDidMount() {
+    let currentcomponent = this;
+    axios.get('http://localhost:3000')
+    .then(function(res){
+      currentcomponent.setState({allentries: res.data}, function() {
+      });
+    })
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const entry = {
+      id: this.state.id,
+      title: this.state.title,
+      fulldescription: this.state.fulldescription,
+      locationraw: this.state.locationraw,
+      locationnormalized: this.state.locationnormalized,
+      contracttype: this.state.contracttype,
+      contracttime: this.state.contracttime,
+      company: this.state.company,
+      category: this.state.category,
+      sourcename: this.state.sourcename
+    }
+    
+    axios.post('http://localhost:3000', entry)
+    .then(res => {
+        // const persons = res.data;
+        // this.setState({ persons });
+      })         
+  
+} 
+  render() {
+    return (
+    <div className="App">
+      <form onSubmit={this.onSubmit} method="user" className="right">
+        <span>id:</span>
+        <input type="text" name="id" id="id" onChange={this.handleIdChange}/>
+        <label>
+        <span>title:</span>
+        <input type="text" name="title" onChange={this.handleTitleChange}/>
+        </label>
+        <label>
+        <span>full description:</span>
+        <input type="text" name="full description" onChange={this.handleFullDescriptionChange}/>
+        </label>
+        <label>
+        <span>location raw:</span>
+        <input type="text" name="location raw" onChange={this.handleLocationRawChange}/>
+        </label>
+        <label>
+        <span>location normalized:</span>
+        <input type="text" name="location normalized"onChange={this.handleLocationNormalizedChange} />
+        </label>
+        <label>
+        <span>contract type:</span>
+        <input type="text" name="contract type" onChange={this.handleLocationNormalizedChange} />
+        </label>
+        <label>
+        <span>contract time:</span>
+        <input type="text" name="contract time" onChange={this.handleContractTimeChange} />
+        </label>
+        <label>
+        <span>company:</span>
+        <input type="text" name="company" onChange={this.handleCompanyChange} />
+        </label>
+        <label>
+        <span>category:</span>
+        <input type="text" name="category" onChange={this.handleCategoryChange} />
+        </label>
+        <label>
+        <span>source name:</span>
+        <input type="text" name="source name" onChange={this.handleSourceNameChange} />
+        </label>
+        <input type="submit" value="insert" />
+      </form>
+    {/* ===================== */}
+    {/* HOW TO USE IT         */}
+    {/* ===================== */}
+    <JsonToTable json={this.state.allentries} />
+    {/* ===================== */}
+  </div>
+  );
+  }
+}
+export default App;
